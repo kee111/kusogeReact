b1709efefacc508abe977b38b26ab781a057a204
kee111 <eqrlgrey@gmail.com>
2021-10-14 21:24:00 +0900

    謎エラー

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from "react";
-import Fighter from "./components/ Fighter";
 import "./App.scss";
+import Fighter from "./components/ Fighter";
 import Teki from "./components/Teki";
 import Header from "./components/Header";
+import Command_area from "./components/Command_area";
 
+// 関数たち
 const functions = {
     changeHP_fighter: (newFigher, fighterName, hp) => {
         return newFigher.map((fighter) => {
@@ -17,13 +19,17 @@ const functions = {
 
 // main
 function App() {
+    // ファイターとゴリラの情報
     const [teki, setTeki] = useState({ name: "super_gorira", hp: 3000 });
     const [fighter, setFighter] = useState([
         { name: "hyper_yellow", hp: 200, mp: 100 },
         { name: "fire_red", hp: 200, mp: 100 },
         { name: "speed_blue", hp: 200, mp: 100 },
     ]);
+    // コマンドエリアの情報
+    // const [command, setCommand] = useState("|");
 
+    // ファイターのhpを操作するサンプルコード
     const newFigher = Object.assign([], fighter);
     useEffect(() => {
         setFighter(functions.changeHP_fighter(newFigher, "fire_red", 5000));
@@ -46,7 +52,7 @@ function App() {
             <Header />
             <div className="teki_area">{teki_list}</div>
             <div className="mikata_area">{fighter_list}</div>
-            <div className="command_area">command_area</div>
+            <Command_area />
         </div>
     );
 }
---
+++ src/components/Command.js
@@ -0,0 +1,9 @@
+import "./command.scss";
+
+export default function Command_area() {
+    return (
+        <div className="command_wrapper">
+            <div className="command_area">command_area</div>
+        </div>
+    );
+}
